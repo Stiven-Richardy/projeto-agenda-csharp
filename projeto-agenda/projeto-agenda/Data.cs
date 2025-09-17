c251c333e9b4d45c546afdb2001d26ed998aab63
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-09-16 21:58:00 -0300

    Inicializando classes

--- projeto-agenda/projeto-agenda/Data.cs
+++ projeto-agenda/projeto-agenda/Data.cs
@@ -8,5 +8,31 @@ namespace projeto_agenda
 {
     internal class Data
     {
+        private int dia;
+        private int mes;
+        private int ano;
+
+        public int Dia { get => dia; set => dia = value; }
+        public int Mes { get => mes; set => mes = value; }
+        public int Ano { get => ano; set => ano = value; }
+
+        public Data(int dia, int mes, int ano)
+        {
+            Dia = dia;
+            Mes = mes;
+            Ano = ano;
+        }
+
+        public void setData(int dia, int mes, int ano)
+        {
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        }
+
+        public override string ToString()
+        {
+            return this.Dia + "/" + this.Mes + "/" + this.Ano;
+        }
     }
 }
